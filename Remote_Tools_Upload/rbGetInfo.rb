1de29d9e2eb22eb2855fbc92390026fb82877680
Mark James <snar5@yahoo.com>
2024-12-02 14:38:40 -0600

    Update rbGetInfo.rb

--- Remote_Tools_Upload/rbGetInfo.rb
+++ Remote_Tools_Upload/rbGetInfo.rb
@@ -1,47 +1,35 @@
+     
 ##
 # A ruby program to provide some small functions to lookup information about websites 
 # I found it useful in cases where dirb or DirBuster would error out. 
 #  
 # Author: mthorburn 12/29/2014 
-#
-# Requires gem whois and ipaddr 
+# Updated in 2024 ten years wow
 # 
 ## 
 
-require 'nokogiri'
+
 require 'rubygems'
 require 'uri'
 require 'openssl'
 require 'net/https'
-require 'getopt/long'
 require 'thread'
 require 'ipaddr' 
+require 'optparse'
 
 
 def usage()
 
 	puts "Usage:\n "
+    puts
 	puts " ruby rbGetInfo.rb [options]"
-	puts ""
-	puts "Main 'modes'" 
-	puts " --title           Grabs title and response  information for a selected IP and can return hearder with -e option"
-	puts " --direnum         Will cycle through a list of directories and return a code *dirbuster-ish*"
-	puts "\n" 
-	puts "title options --title\n"
-	puts " --host   '-h'	 Do a single lookup for one host"
-	puts " --file   '-f'	 Use a file for host (ip) input will grab a title for each page"
-	puts " --out    '-o'	 output to a file (and onscreen) "
-	puts " --ssl    '-s'	 Uses SSL for connection "
-	puts " --expand '-e'	 Include headers from request"
-	puts "\n\tex. rbGetInfo.rb --title -h google.com \n\n"
-	puts "direnum options --direnum\n"
-	puts " --host    '-h'	 Do a single lookup for one host "
-	puts " --basedir '-b'	 The base directory to start "
-	puts " --file    '-f'	 Use a file for directory inputs"
-	puts " --ssl     '-s'	 Use an SSL connection" 
-	puts " --out     '-o' 	 Output to a file " 
-	puts "\n\tex. rbGetInfo.rb --direnum -h google.com --basedir /images --output list.out\n\n"
-	puts "\n "
+    puts
+	puts " -h --host  Do a single lookup for one host"
+	puts " -i --file  Use a file for host (ip) input will grab a title for each page"
+	puts " -o --out   Output to a file (and onscreen) "
+	puts " -s --ssl   Uses SSL for connection "
+    puts " -p --port  Port to Use"
+	puts "\n\tex. rbGetInfo.rb -h google.com -s -o output.txt\n\n"
 	return 
 
 
@@ -52,34 +40,24 @@ def getargs
 	       	usage	
 		exit 0
 	end 
-	include Getopt
+
 	begin	
-	opt = Long.getopts(
-		["--title", OPTIONAL],
-		["--direnum", OPTIONAL],
-    		["--host", "-h", REQUIRED],
-		["--ssl", "-s",BOOLEAN],
-		["--expanded","-e",BOOLEAN],
-		["--file","-f",REQUIRED],
-		["--port","-p",REQUIRED],
-		["--basedir","-b",REQUIRED],
-		["--out", "-o",REQUIRED],
-		["--whois", OPTIONAL],
-		["--domain", "", REQUIRED],
-		["--net", "", REQUIRED],
-		["--type",REQUIRED],
-		["--name", REQUIRED],
-		["--pocid",REQUIRED],
-		["--help",OPTIONAL]
-		)
+    options = {}
+        OptionParser.new do |opt|
+        opt.banner = "Usage: rbGetTitle.rb [options]"
+          opt.on('-h', '--host HOST','single host') { |o| options[:HOST] = o }
+          opt.on('-i', '--[no-]input_file INPUTFILE', 'filename') { |o| options[:INPUTFILE] = o }
+          opt.on('-p', '--port PORT', 'port number') { |o| options[:PORT] = o }
+          opt.on('-o', '--outfile OUTFILE', 'output file') { |o| options[:OUTFILE] = o }
+          opt.on('-s', "--[no-]ssl", 'use ssl') { |o| options[:SSL] = o }
+        end.parse!
 	rescue Exception => e 
-	#	usage 
+		usage 
 		puts e
-	
 		exit 
 	end 
-	return opt
-
+	return options
+    
 end 
 
 class Colors
@@ -102,7 +80,6 @@ end # End color class
 
 
 def out_to_file(response,outfile)
-	puts "Wrote #{response}"
 	output = File.open(outfile,"a")
 	output.write(response)
 	output.close 
@@ -113,31 +90,30 @@ class Request
 
 	attr_reader :file_formatted, :response , :success, :responsebody 
 
-	def initialize (aURL, aSSL, aExpanded,aPort=80,aBaseDir)
+	def initialize (aURL, aSSL,aPort=80)
 		begin
 			@url = aURL
 			@ssl = aSSL
-			@incheader = aExpanded
 			@responsebody  = '' 
 			@response = ''
-			@basedir = aBaseDir
-			@basedir = @basedir.nil? ? "/" : aBaseDir
-			if @ssl 
-				@port = 443 
+            @port = aPort
+			if @ssl
 				@header = 'https://'
 			else 
-				@port = aPort
 				@header = 'http://'
 			end 
-	
-		rescue
+         
+            
+		rescue Exception => e 
 			puts "Error during request setup stage: " << e.message
 		end 
 	end 
 	def send
-		full_path = @header << @url << @basedir
+		full_path = @header << @url 
 		uri = URI.parse(full_path) 
+        
 		http = Net::HTTP.new(uri.host, @port)
+    
 		request = Net::HTTP::Get.new(uri.request_uri)
                 request["User-Agent"] ="Mozilla/5.0"
                 request["Connection"] = "keep-alive"
@@ -150,7 +126,7 @@ class Request
 		begin
 			response = http.request(request)
 		rescue
-			@response = "\033[31mConnection Error: Couldn't Connect to  #{@url}:#{@port}#{@basedir} \033[0m" 
+			@response = "\033[31mConnection Error: Couldn't Connect to  #{full_path}:#{@port} check port and protocol.  \033[0m"
 			@success = false 
 			return
 		end
@@ -161,12 +137,7 @@ class Request
 				title = doc.css('title').text unless doc.css('title').text.nil? 	
 				@response += "\033[32m[+] [#{response.code}] Host: #{@url}:#{@port}#{@basedir}:\033[34m #{title} \33[0m\n"
 				@file_formatted  = "[#{response.code}] Host: #{@url}:#{@port}#{@basedir}:" << title << "\n"
-				if @incheader then 
-					response.each do |x|
-						@response += "  " + x + ": " << response[x] << "\n"
-						@file_formatted += "  " + x + ": " << response[x] << "\n" 
-					end 
-				end	
+				
 				@success = true
 				@responsebody = response.body 
 
@@ -184,171 +155,111 @@ class Request
 
 
 
-end # End of Request Class l
-#########################################################################################
-#                                                                                       #
-# Get Title of Webpage and header information (Optional) input/output to file (Optional)#
-#                                                                                       #
-#########################################################################################
+end # End of Request Class
+
 
 def getTitle(options)
 
+# Local Variables 
+    site = nil
+    port = 80
+    output = false
+    input = nil
+    ssl = false 
+    inputfile = ''
+    outputfile = '' 
 
-site = nil
-expand = false
-port = 80
-basedir='/'
-output = false
-input = nil
-ssl = false 
-filein = 'file.in'
-fileout = 'file.out' 
-
-
-options.each do | opt, arg| 
-	case opt
-		when "host"
-			site = arg
-		when "ssl"
-			ssl = arg
-		when "port"
-			port = arg
-		when "output"
-			output = arg
-		when "file"
-			input = arg
-		when "out"
-			fileout = arg 
-		when "basedir"
-			basedir = arg 
-		
-	end 
-end
+#Parse Command line options 
+    options.each do | opt, arg| 
+	    case opt.to_s
+		    when "HOST"
+			    site = arg
+		    when "ssl"
+			    ssl = arg
+		    when "PORT"
+			    port = arg
+		    when "OUTFILE"
+			    outputfile = arg
+		    when "INPUTFILE"
+                inputfile = arg
+            when "SSL"
+                ssl = arg
+	        end 
+    end
 
 #Check to make sure we have either site or input file
  if site.nil? 
-	if input.nil?  
+	if inputfile.nil?  
 	 	puts "No Site or File Specified!\n\n"
 	 	exit	
 	end
  end
 #Check to make sure we don't have both 
- if !site.nil? && !input.nil?
+ if !site.nil? && !inputfile.nil?
 	puts "Please check your options you can only have a host OR a file input!\n\n"
 	exit
  end
 		
 #Setup Local Count Variables to return 
- 	total_requests = 0 
-	counts = 0 
+    total_requests = 0 
+    counts = 0 
 
-  if !input # Single Request
-	req = Request.new(site,options["ssl"],options["expanded"],options["port"],options["basedir"])
+  if inputfile.nil?  # Single Request
+    puts "Running Single Request "
+	req = Request.new(site,ssl,port)
 	req.send
         total_requests += 1           
         if req.success
-              if fileout
-                 out_to_file(req.file_formatted, fileout)
-              end
               puts req.response
+              puts
               counts += 1
-         else
+        else
               puts "Something Happened, couldn't connect"
               puts req.response
-         end
+              puts
+        end
 
   else # Input hosts from file 
-	begin
-         File.foreach(input) {|x|
-               req = Request.new(x.strip,ssl,options["expanded"],port,basedir)
-               req.send
-               total_requests += 1
-               if req.success
-                  if fileout
-			 out_to_file( req.file_formatted, fileout) 
-               	  end
-               	  puts req.response
-                  counts += 1
-               end
-	       if not req.success 
-		  puts req.response
-	       end 
-		}
-	rescue Exception => e 
-		puts "Error reading file input: #{e}" 
-	end 
+    
+    if !File.file?(inputfile) 
+        puts "Oops, can't open the file, #{inputfile}"
+        usage 
+        exit 
+    else 
+	    begin
+            puts "Running from file #{inputfile}"
+            allrequests = 0 
+            File.foreach(inputfile) {|x|
+                req = Request.new(x.strip,ssl,port)
+                req.send
+                total_requests += 1
+                if req.success
+                    if outputfile.length > 0
+			            out_to_file( req.file_formatted, outputfile ) 
+               	    end
+               	    puts req.response
+                    counts += 1
+                end
+	            if not req.success 
+		            puts req.response
+	            end 
+            allrequests +=1 
+		    }
+	    rescue Exception => e 
+		    puts "Error reading file input: #{e}" 
+	    end 
+    end 
   end 
-	return counts,total_requests 
+	return allrequests,counts,total_requests 
+
 end
 
-#########################################################################################
-#                                                                                       #
-# Enumerate Directories from file or individually                                       #
-#                                                                                       #
-#########################################################################################
-def direnum(options)
-	total_requests = 0
- 	counts = 0
 
- 	if options["file"] then
-                 File.foreach(options["file"]) {|x|
-		  	basedir = "/" << x.strip 
-                 	
-			req = Request.new(options["host"],options["ssl"],options["expanded"],options["port"],basedir)
-                  req.send
-                  total_requests += 1
-                  if req.success
-                         if options["out"]
-                                if File.owned?(options["out"])
-                                        output = File.open(options["out"], "a")
-                                        output << req.response
-                                        output.close
-                                else
-                                         output = File.open(options["out"], "w")
-                                         output << req.response
-                                         output.close
-                                end
-                         end
-                         puts req.response
-                         counts += 1
-                  end
-                  if not req.success
-                        puts req.response
-                  end
-                         }
-         else
-
-                 req = Request.new(options["host"],options["ssl"],options["expanded"],options["port"],options["basedir"])
-                 req.send
-                 total_requests += 1
-
-                         if req.success
-                                 if options["out"]
-                                         out_to_file(req.file_formatted, options["out"])
-                                 end
-                                 puts req.response
-                                counts += 1
-                         end
-                        if not req.success
-                                puts "Something Happened, couldn't connect"
-                                puts req.response
-                        end
-
-         end
-
-        return counts,total_requests
- 
-	
-end
  
 def display_banner
 	system("clear")
 	puts "\033[31m
---------------------------------------------
-
-Get Title and Header information (-e) 
-
---------------------------------------------- 
+    Grabbing Website(s) Titles
 	\n\033[0m"
 
 end
@@ -359,21 +270,12 @@ def start
 	display_banner
 
 	options = getargs # Get Command line options 
-	    options.each do | opt, arg|
-		case opt
-		when "title"
-	 	counts,total_requests = getTitle(options)
-		when "direnum"
-		counts,total_requests =  direnum(options)
-		when "help"
-		usage
-		end
-    	end
-	
+	allrequests,counts,total_requests = getTitle(options)
+	puts ""
+    puts "Finished collecting #{counts} website(s) out of #{allrequests} attempted."
 end 
 
 
 # Start of the main program 				
 start		
-
 
